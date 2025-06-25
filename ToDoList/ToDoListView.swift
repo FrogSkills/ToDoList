cede6219bfa17bff35c42bc849c2510d1038b521
Miguel <mgamez20@icloud.com>
2025-06-25 15:51:09 -0400

    Added headers, learned multiple list formats, gained more knowledge on navigation titles, passed static values  between views, corrected problems with preview provided so it would display a another view in a file. Worked with ForEach in lists.

--- ToDoList/ToDoListView.swift
+++ ToDoList/ToDoListView.swift
@@ -8,20 +8,35 @@
 import SwiftUI
 
 struct ToDoListView: View {
+    var toDos = ["Learn Swift",
+                 "Build Apps",
+                 "Change my World",
+                 "Bring the Awesome",
+                 "Take a Vacation"]
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                NavigationLink {
-                    DetailView()
-                } label: {
-                    Image(systemName: "eye")
-                    Text("Show the New View!")
+            List {
+                ForEach(toDos, id: \.self) { toDo in
+                    NavigationLink {
+                        DetailView(passedValue: toDo)
+                    } label: {
+                        Text(toDo)
+                    }
                 }
-                .buttonStyle(.borderedProminent)
+                
+//                ForEach(0..<100, id: \.self) { number in
+//                    NavigationLink {
+//                        DetailView(passedValue: "Item \(number)")
+//                    } label: {
+//                        Text("Item \(number)")
+//                    }
+//                }
             }
-            .padding()
+            .navigationTitle("To Do List") // Back button takes this name automatically.
+            .navigationBarTitleDisplayMode(.inline)
+            .listStyle(.plain)
         }
-        
     }
 }
 
