977ad93bc4ee16daa297bdc8a5080255bfeacc67
Miguel <mgamez20@icloud.com>
2025-06-25 19:25:10 -0400

    Used fullScreenCover instead of sheet, but apple prefers sheet as defult. Covered toolbars and how to embed them in a NavigationStack when needed.

--- ToDoList/ToDoListView.swift
+++ ToDoList/ToDoListView.swift
@@ -14,15 +14,18 @@ struct ToDoListView: View {
                  "Bring the Awesome",
                  "Take a Vacation"]
     
+    @State private var sheetIsPresented = false
+    
     var body: some View {
         NavigationStack {
             List {
                 ForEach(toDos, id: \.self) { toDo in
                     NavigationLink {
-                        DetailView(passedValue: toDo)
+                        DetailView(toDo: toDo)
                     } label: {
                         Text(toDo)
                     }
+                    .font(.title2)
                 }
                 
 //                ForEach(0..<100, id: \.self) { number in
@@ -36,6 +39,21 @@ struct ToDoListView: View {
             .navigationTitle("To Do List") // Back button takes this name automatically.
             .navigationBarTitleDisplayMode(.inline)
             .listStyle(.plain)
+            // Below we can use fullScreenCover instead of sheet, but apple prefers sheets and would be best for future updates.
+            .sheet(isPresented: $sheetIsPresented) {
+                NavigationStack {
+                    DetailView(toDo: "")
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button() {
+                        sheetIsPresented.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
         }
     }
 }
